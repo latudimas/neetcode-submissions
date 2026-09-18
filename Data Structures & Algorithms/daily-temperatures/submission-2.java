f4bd20102b725e22f2c7c0b7a5ecaecda5b007f2
Riswandha Latu Dimas <riswandha.ld@gmail.com>
2026-09-18 23:38:59 +0700

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    private record Entry(int temp, int idx) {}
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        // Improvement SOLUTION
+        Deque<Entry> stack = new ArrayDeque<>();
+        stack.push(new Entry(temperatures[0], 0));
+
+        int[] output = new int[temperatures.length];
+
+        for (int i = 1; i < temperatures.length; i++){
+            // while current temp > stack top
+            while (!stack.isEmpty() && stack.peek().temp() < temperatures[i]) {
+                output[stack.peek().idx()] = i - stack.peek().idx(); //calculate distance
+                stack.pop();
+            } 
+                stack.push(new Entry(temperatures[i], i));
+        }
+        return output;
+    }
+}
